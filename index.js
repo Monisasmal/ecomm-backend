f4ab66e4ae312dd6f272787b18c67c2b827fed17
manisasmal5597@gmail.com <manisasmal5597@gmail.com>
2026-02-02 18:05:03 +0530

    Update CORS and Port for production

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: "https://react-ecommerce-project-manaswini-sasmals-projects.vercel.app/" // Replace with your ACTUAL Vercel link
+}));
 app.use(express.json());
 
 // 1. PASTE YOUR COPIED LINK HERE
@@ -61,7 +63,7 @@ app.get('/api/singleproduct/:id', async (req, res) => {
     }
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`🚀 Server is running on http://localhost:${PORT}`);
 });
